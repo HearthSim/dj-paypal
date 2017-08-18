9734b8415ee78bfca2332d61754362f092859f81
Jerome Leclanche <jerome@leclan.ch>
2017-08-18 13:09:02 +0300

    Implement sdk configuration in djpaypal.settings

---
+++ djpaypal/settings.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+
+PAYPAL_MODE = getattr(settings, "PAYPAL_MODE", None)
+PAYPAL_CLIENT_ID = getattr(settings, "PAYPAL_CLIENT_ID", None)
+PAYPAL_CLIENT_SECRET = getattr(settings, "PAYPAL_CLIENT_SECRET", None)
+
+
+PAYPAL_SETTINGS = {
+	"mode": PAYPAL_MODE,
+	"client_id": PAYPAL_CLIENT_ID,
+	"client_secret": PAYPAL_CLIENT_SECRET,
+}
+
+if PAYPAL_CLIENT_ID and PAYPAL_CLIENT_SECRET:
+	from paypalrestsdk import configure
+	configure(PAYPAL_SETTINGS)
