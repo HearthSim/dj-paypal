290dc60e0dcde3e51a8e44acaf0efdd6f35f23b1
Jerome Leclanche <jerome@leclan.ch>
2017-08-25 20:21:23 +0300

    Implement djpaypal_create_plans command

---
+++ djpaypal/management/commands/djpaypal_create_plans.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.core.management import BaseCommand
+from djpaypal import models
+
+
+class Command(BaseCommand):
+	help = "Create plans on Paypal from settings.PAYPAL_PLANS"
+
+	def handle(self, *args, **options):
+		for name, plan in settings.PAYPAL_PLANS.items():
+			self.stdout.write("Creating plan %r..." % (name))
+			obj = models.BillingPlan.create(plan, activate=True)
+			self.stdout.write("Created plan: %r (%r)" % (obj, obj.id))
