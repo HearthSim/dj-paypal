81b9cbb1c41e0e5642143abf2ddbcd82c8b700ef
Jerome Leclanche <jerome@leclan.ch>
2017-08-31 15:17:11 +0300

    Save executed_at on PreparedBillingAgreement before attempting execution

--- djpaypal/models/billing.py
+++ djpaypal/models/billing.py
@@ -140,12 +140,20 @@ class PreparedBillingAgreement(models.Model):
 		self.save()
 
 	def execute(self):
+		"""
+		Execute the PreparedBillingAgreement by creating and executing a
+		matching BillingAgreement.
+		"""
+		# Save the execution time first.
+		# If execute() fails, executed_at will be set, with no executed_agreement set.
+		self.executed_at = now()
+		self.save()
+
 		with transaction.atomic():
 			ret = BillingAgreement.execute(self.id)
 			ret.user = self.user
 			ret.save()
 			self.executed_agreement = ret
-			self.executed_at = now()
 			self.save()
 
 		return ret
