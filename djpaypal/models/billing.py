f18a38e3819f2e0129d456138bf8b11e307b599c
Jerome Leclanche <jerome@leclan.ch>
2017-09-03 00:11:37 +0300

    Do not attempt to add charge_models if they are missing

--- djpaypal/models/billing.py
+++ djpaypal/models/billing.py
@@ -235,9 +235,10 @@ class PaymentDefinition(PaypalObject):
 		id, cleaned_data, m2ms = super().clean_api_data(data)
 
 		cleaned_data["frequency"] = cleaned_data["frequency"].upper()
-		charge_models = cleaned_data.pop("charge_models")
-		# Sync payment definitions but do not fetch them (we have them in full)
-		m2ms["charge_models"] = ChargeModel.objects.sync_data(charge_models, fetch=False)
+		if "charge_models" in cleaned_data:
+			charge_models = cleaned_data.pop("charge_models")
+			# Sync payment definitions but do not fetch them (we have them in full)
+			m2ms["charge_models"] = ChargeModel.objects.sync_data(charge_models, fetch=False)
 
 		return id, cleaned_data, m2ms
 
