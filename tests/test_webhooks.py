6e7134f0e49ebace9b6dc4fae23fb5186f18dada
Jerome Leclanche <jerome@leclan.ch>
2017-09-05 03:58:33 +0300

    Clean up repetitions in webhook tests

--- tests/test_webhooks.py
+++ tests/test_webhooks.py
@@ -4,13 +4,17 @@ from djpaypal import models
 from .conftest import get_fixture
 
 
-@pytest.mark.django_db
-def test_webhook_billing_plan_created():
-	data = get_fixture("webhooks/billing.plan.created.json")
-	resource = data["resource"]
+def get_webhook_from_fixture(event_type):
+	data = get_fixture("webhooks/{event_type}.json".format(event_type=event_type))
 	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
 	webhook.save()
 	webhook.process()
+	return data, data["resource"], webhook
+
+
+@pytest.mark.django_db
+def test_webhook_billing_plan_created():
+	data, resource, webhook = get_webhook_from_fixture("billing.plan.created")
 	assert webhook.webhook_event.id == data["id"]
 	assert webhook.webhook_event.resource["id"] == resource["id"]
 	assert models.BillingPlan.objects.get(id=resource["id"])
@@ -18,22 +22,14 @@ def test_webhook_billing_plan_created():
 
 @pytest.mark.django_db
 def test_webhook_billing_subscription_created_then_cancelled():
-	data = get_fixture("webhooks/billing.subscription.created.json")
-	resource = data["resource"]
-	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
-	webhook.save()
-	webhook.process()
+	data, resource, webhook = get_webhook_from_fixture("billing.subscription.created")
 	assert webhook.webhook_event.id == data["id"]
 	assert webhook.webhook_event.resource["id"] == resource["id"]
 	assert models.BillingAgreement.objects.get(id=resource["id"])
 	assert models.BillingAgreement.objects.get(id=resource["id"]).state == "Pending"
 
 	# Cancel the subscription
-	data = get_fixture("webhooks/billing.subscription.cancelled.json")
-	resource = data["resource"]
-	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
-	webhook.save()
-	webhook.process()
+	data, resource, webhook = get_webhook_from_fixture("billing.subscription.cancelled")
 	assert webhook.webhook_event.id == data["id"]
 	assert webhook.webhook_event.resource["id"] == resource["id"]
 	assert models.BillingAgreement.objects.get(id=resource["id"]).state == "Cancelled"
@@ -41,11 +37,7 @@ def test_webhook_billing_subscription_created_then_cancelled():
 
 @pytest.mark.django_db
 def test_webhook_customer_dispute_created():
-	data = get_fixture("webhooks/customer.dispute.created.json")
-	resource = data["resource"]
-	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
-	webhook.save()
-	webhook.process()
+	data, resource, webhook = get_webhook_from_fixture("customer.dispute.created")
 	assert webhook.webhook_event.id == data["id"]
 	assert webhook.webhook_event.resource["dispute_id"] == resource["dispute_id"]
 	assert models.Dispute.objects.get(id=resource["dispute_id"])
@@ -53,10 +45,7 @@ def test_webhook_customer_dispute_created():
 
 @pytest.mark.django_db
 def test_webhook_risk_dispute_created():
-	data = get_fixture("webhooks/risk.dispute.created.json")
-	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
-	webhook.save()
-	webhook.process()
+	data, resource, webhook = get_webhook_from_fixture("customer.dispute.created")
 	assert webhook.webhook_event.id == data["id"]
 	# TODO: ensure actual Dispute object is created
 	# Depends on https://github.com/paypal/PayPal-Python-SDK/issues/216
@@ -64,11 +53,7 @@ def test_webhook_risk_dispute_created():
 
 @pytest.mark.django_db
 def test_webhook_payment_sale_completed():
-	data = get_fixture("webhooks/payment.sale.completed.json")
-	resource = data["resource"]
-	webhook = models.WebhookEventTrigger(headers={}, body=json.dumps(data))
-	webhook.save()
-	webhook.process()
+	data, resource, webhook = get_webhook_from_fixture("payment.sale.completed")
 	assert webhook.webhook_event.id == data["id"]
 	assert webhook.webhook_event.resource["id"] == resource["id"]
 	assert models.Sale.objects.get(id=resource["id"])
