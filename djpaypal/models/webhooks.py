4e9af5eacdc5c5084238015881c71de84f5fabf0
Jerome Leclanche <jerome@leclan.ch>
2017-09-05 03:59:19 +0300

    Implement a webhook_handler decorator

--- djpaypal/models/webhooks.py
+++ djpaypal/models/webhooks.py
@@ -1,4 +1,5 @@
 import json
+from fnmatch import fnmatch
 from traceback import format_exc
 from django.db import models
 from django.dispatch import Signal
@@ -229,3 +230,55 @@ class WebhookEventTrigger(models.Model):
 		if save:
 			self.save()
 		return obj
+
+
+_registrations = {}
+
+
+def webhook_handler(*event_types):
+	"""
+	Decorator that registers a function as a webhook handler.
+
+	Usage examples:
+
+	>>> # Hook a single event
+	>>> @webhook_handler("payment.sale.completed")
+	>>> def on_payment_received(event):
+	>>>     payment = event.get_resource()
+	>>>     print("Received payment:", payment)
+
+	>>> # Multiple events supported
+	>>> @webhook_handler("billing.subscription.suspended", "billing.subscription.cancelled")
+	>>> def on_subscription_stop(event):
+	>>>     subscription = event.get_resource()
+	>>>     print("Stopping subscription:", subscription)
+
+	>>> # Using a wildcard works as well
+	>>> @webhook_handler("billing.subscription.*")
+	>>> def on_subscription_update(event):
+	>>>     subscription = event.get_resource()
+	>>>     print("Updated subscription:", subscription)
+	"""
+
+	# First expand all wildcards and verify the event types are valid
+	event_types_to_register = set()
+	for event_type in event_types:
+		if "*" in event_type:
+			# expand it
+			for t in WEBHOOK_EVENT_TYPES:
+				if fnmatch(t, event_type):
+					event_types_to_register.add(t)
+		elif event_type not in WEBHOOK_EVENT_TYPES:
+			raise ValueError("Unknown webhook event: %r" % (event_type))
+		else:
+			event_types_to_register.add(event_type)
+
+	# Now register them
+	def decorator(func):
+		for event_type in event_types_to_register:
+			if event_type not in _registrations:
+				_registrations[event_type] = []
+			_registrations[event_type].append(func)
+		return func
+
+	return decorator
