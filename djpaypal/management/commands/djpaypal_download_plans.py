4fc00e3bcc3b08e1b9f90c25848ebb7a96a38a2c
Jerome Leclanche <jerome@leclan.ch>
2017-09-30 21:29:43 +0300

    Fix a potential crash in djpaypal_download_plans

--- djpaypal/management/commands/djpaypal_download_plans.py
+++ djpaypal/management/commands/djpaypal_download_plans.py
@@ -10,4 +10,6 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		for status in ("created", "active"):
 			all_plans = paypal_models.BillingPlan.all({"status": status})
+			if all_plans.plans is None:
+				continue
 			models.BillingPlan.objects.sync_data(all_plans.plans)
