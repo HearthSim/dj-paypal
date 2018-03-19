e5f7cec40d3c390a0d81213f535dd085a0716198
Jerome Leclanche <jerome@leclan.ch>
2018-03-19 15:04:13 +0200

    Copy djstripe.enums.Enum to drop the dependency on dj-stripe

--- djpaypal/enums.py
+++ djpaypal/enums.py
@@ -1,5 +1,51 @@
+import operator
+from collections import OrderedDict
+
 from django.utils.translation import ugettext as _
-from djstripe.enums import Enum
+
+
+class EnumMetaClass(type):
+	@classmethod
+	def __prepare__(self, name, bases):
+		return OrderedDict()
+
+	def __new__(self, name, bases, classdict):
+		members = []
+		keys = {}
+		choices = OrderedDict()
+		for key, value in classdict.items():
+			if key.startswith("__"):
+				continue
+			members.append(key)
+			if isinstance(value, tuple):
+				value, alias = value
+				keys[alias] = key
+			else:
+				alias = None
+			keys[alias or key] = key
+			choices[alias or key] = value
+
+		for k, v in keys.items():
+			classdict[v] = k
+
+		classdict["__choices__"] = choices
+		classdict["__members__"] = members
+
+		# Note: Differences between Python 2.x and Python 3.x force us to
+		# explicitly use unicode here, and to explicitly sort the list. In
+		# Python 2.x, class members are unordered and so the ordering will
+		# vary on different systems based on internal hashing. Without this
+		# Django will continually require new no-op migrations.
+		classdict["choices"] = tuple(
+			(str(k), str(v))
+			for k, v in sorted(choices.items(), key=operator.itemgetter(0))
+		)
+
+		return type.__new__(self, name, bases, classdict)
+
+
+class Enum(metaclass=EnumMetaClass):
+	pass
 
 
 class PaypalBool(Enum):
